2c98888f4f704134c7d3883bae86173178cc522c
Michelle <michelleannecampbell@gmail.com>
2017-12-18 20:12:54 -0500

    final

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientID =  'd35670ba65c84bfa89c7cbca215130b8';
-const redirectURI = 'http://localhost:3000/';
+const redirectURI = 'http://curate-your-list.surge.sh/';
 
 
 
@@ -35,9 +35,8 @@ const Spotify = {
                         });
                     });
             });
-    }
-};
-  search (term) {
+    },
+  search(term){
        const accessToken = Spotify.getAccessToken();
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {
