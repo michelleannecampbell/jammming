a773fe6e9a77a1620189b2a4027b9d79aece037b
Michelle <michelleannecampbell@gmail.com>
2017-12-18 21:22:56 -0500

    try

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -8,34 +8,18 @@ let accessToken ;
 
 const Spotify = {
 
-  savePlaylist(name, trackUris) {
-        if (!name || !trackUris.length) {
-            return;
-        }
+  savePlaylist(name, trackURIs) {
+       if (name != null || trackURIs.length > 0) {
+         const accessToken = this.getAccessToken();
+         const headers = { 'Authorization': 'Bearer ' + accessToken }
+         let userID;
 
-        const accessToken = Spotify.getAccessToken();
-        const headers = { Authorization: `Bearer ${accessToken}` };
-        let userId;
-
-        return fetch('https://api.spotify.com/v1/me', { headers: headers }
-        ).then(response => response.json()
-            ).then(jsonResponse => {
-                userId = jsonResponse.id;
-                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-                    headers: headers,
-                    method: 'POST',
-                    body: JSON.stringify({ name: name })
-                }).then(response => response.json()
-                    ).then(jsonResponse => {
-                        const playlistId = jsonResponse.id;
-                        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
-                            headers: headers,
-                            method: 'POST',
-                            body: JSON.stringify({ uris: trackUris })
-                        });
-                    });
-            });
-    },
+         JSON.get({
+           url: 'https://api.spotify.com/v1/me',
+           headers: headers
+         }, response => {
+           userID = response.id;
+         }),
   search(term){
        const accessToken = Spotify.getAccessToken();
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
