86c7d088ff8f0257de44524a4df161db7a0145ad
Michelle <michelleannecampbell@gmail.com>
2017-12-20 14:57:13 -0500

    feature fix

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,28 +2,27 @@ import React, {Component} from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component{
-  constructor(props){
-    super(props)
-    this.search = this.search.bind(this);
-    this.handleTermChange = this.handleTermChange.bind(this);
-  }
+  constructor(props){
+    super(props)
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
 
-  handleTermChange(event){
-    this.setState({term: event.target.value});
+  handleTermChange(event){
+    this.setState({term: event.target.value});
 }
-  search(){
-    this.props.onSearch(this.state.term)
-  }
+  search(event){
+    event.preventDefault();
+    this.props.onSearch(this.state.term);
+  }
 
-    render(){
-      return (
-        <div className="SearchBar">
-        <form onSubmit={this.search}>
-          <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
-          <button type="submit">SEARCH</button>
-        </form>
-      </div>
-      );
-    }
+  render(){
+    return (
+      <form onSubmit={this.search} className="SearchBar">
+          <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+        <button type="submit">SEARCH</button>
+      </form>
+    );
+  }
 }
 export default SearchBar;
